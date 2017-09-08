b390fb6f6c716fbe968a41b37b575379501a37de
usuario <usuario@L-1702033>
2017-09-08 16:24:36 +0200

    Agregar archivos de proyecto.

---
+++ SupermercadoServer/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SupermercadoServer.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SupermercadoServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SupermercadoServer.WebApiApplication" Language="C#" %>
