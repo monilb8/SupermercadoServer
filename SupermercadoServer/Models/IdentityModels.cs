37b2da3c58c5cd79eed36c3e6bcc6ef89cf52620
monica <usuario@L-1702033>
2017-09-12 18:30:28 +0200

    [ThreadStatic] en Identity Models

--- SupermercadoServer/Models/IdentityModels.cs
+++ SupermercadoServer/Models/IdentityModels.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using System.Data.Entity;
+using System;
 
 namespace SupermercadoServer.Models
 {
@@ -21,6 +22,7 @@ namespace SupermercadoServer.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        [ThreadStatic]
         public static ApplicationDbContext applicationDbContext;
 
         public ApplicationDbContext()
